79deb38b9cdb56154931d6f3a8c3ad387198c96f
najafsikander <najafsikander@yahoo.com>
2024-07-10 13:06:00 +0400

    Completed the divison of all of the App page content into separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListing from "./components/JobListing";
+import ViewAllJobs from "./components/ViewAllJobs";
 const App = () => {
-  return(
+  return (
     <>
-    <h1>App Component</h1>
+    <Navbar/>
+    <Hero/>
+    <HomeCards/>
+    <JobListing/>
+    <ViewAllJobs/>
     </>
-  )
-}
+  );
+};
 
 export default App;
