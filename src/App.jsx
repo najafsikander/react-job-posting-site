afaaf7446cacfc8056cdf4abc42d5295a013a95d
najafsikander <najafsikander@yahoo.com>
2024-07-11 12:49:06 +0400

    Completed add new job form with api call

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, {jobLoader} from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
 import MainLayout
  from "./layouts/MainLayout";
+
+import { addJob
+  
+ } from "./services/jobs";
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path="/" element={<MainLayout/>}>
       <Route index element={<HomePage />}/>
       <Route path="/jobs" element={<JobsPage/>}/>
+      <Route path="/jobs/:id" element={<JobPage/>} loader={jobLoader}/>
+      <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>}/>
       <Route path="*" element={<NotFoundPage/>}/>
     </Route>,
   )
